4957e86dec1bed6b0c3dc6b8629b6bdec58e0007
D-A-Fernandez <fd2006711@gmail.com>
2023-11-29 10:10:20 -0500

    AHHHHHHHHHH

---
+++ Assets/Scripts/pickUP.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUP : MonoBehaviour
+{
+    public GameObject objPosition;
+    bool canPickup;
+    public GameObject item;
+    bool hasItem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canPickup = false;
+        hasItem = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canPickup == true) // if youve entered the objects collider
+        {
+            if (Input.GetKeyDown("e")) 
+            {
+                item.GetComponent<Rigidbody>().isKinematic = true;
+                item.transform.position = objPosition.transform.position;
+                item.transform.parent = objPosition.transform;
+            }
+        }
+        if (Input.GetKeyDown("q") && hasItem == true)
+        {
+            item.GetComponent <Rigidbody>().isKinematic = false;
+            item.transform.parent = null;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "item")
+        {
+            canPickup = true;
+            item = other.gameObject;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        canPickup = false;
+    }
+}
