40b734265fbea7f8bbbd4dfc48465dd9be72edd6
shabibokiribo <Niahtaylor806@yahoo.com>
2023-12-07 09:06:00 -0500

    commit

--- Assets/Scripts/Dog.cs
+++ Assets/Scripts/Dog.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 public class Dog : MonoBehaviour
 {
