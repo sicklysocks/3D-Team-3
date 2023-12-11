f804e2315b2a70f6ae41ea54fa4d9a9b81876ffd
Janokye <145355697+Janokye@users.noreply.github.com>
2023-12-11 11:10:58 -0500

    CPS TESTING

---
+++ Assets/Scripts/FieldOfTheView.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfTheView : MonoBehaviour
+{
+    public float radius;
+    [Range(0,36)]
+    public float angle;
+
+    public GameObject Player; //Attach to Player with the tag Player
+
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+
+    public bool canSeePlayer;
+
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player"); //Find Player
+        StartCoroutine(FOVRoutine());
+    }
+
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+        while(true)
+        {
+            yield return wait; // wait 0.2 seconds
+            FieldOfViewCheck();
+        }
+    }
+    private void FieldOfViewCheck() //check for the player
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);//look for player on layermask only. If player is not on layermask it wont work
+        if (rangeChecks.Length != 0)
+        { 
+          Transform target = rangeChecks[0].transform;
+          Vector3 directionToTarget = (target.position - transform.position).normalized; // the direction of where CPS is looking to where Player is
+
+          if(Vector3.Angle(transform.forward, directionToTarget) < angle/2) //Check angle?
+          {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position); //enemey is close enough to Player to trigger FOV
+
+                if(!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask)) 
+                {
+                    canSeePlayer = true;
+                }
+                else
+                {
+                    canSeePlayer = false;
+                }
+          }
+          else //player is not in FOV
+          {
+                canSeePlayer = false;
+          }
+        }
+        else if (canSeePlayer) //player is no longer in the view of CPS
+        {
+            canSeePlayer = false; 
+        }
+    }
+}
+
+
+// YOUTUBE VIDEO: https://www.youtube.com/watch?v=j1-OyLo77ss
+// 21:14/23:44
